a20a15f0eabcb25e93e6dbe6635460cfd019af1e
albert-axi <albert@academyxi.com>
2024-07-19 19:48:19 +0930

    create db

---
+++ server/migrations/versions/7a2d2ec93b08_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: 7a2d2ec93b08
+Revises: 
+Create Date: 2024-07-19 19:47:14.825553
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7a2d2ec93b08'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
